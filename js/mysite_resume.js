d12abb89bce0ab555e65e546843bbbe90f23208e
David R. Jex <david.jex@me.com>
2017-07-15 21:59:59 -0500

    Animations added to Javascript, CSS, and PHP selections.

--- js/mysite_resume.js
+++ js/mysite_resume.js
@@ -23,3 +23,39 @@ $('#css').hover(
 		$('#character_box').html('<img src="/img/front_facing.png" alt="character" id="character">');
 	}
 );
+$('#javascript').hover( 
+	function() {
+		$(this).css('background-color','#fff');
+		$(this).css('color', '#000');
+		$('#character_box').html('<img src="/img/fireball_spell.gif" alt="character" id="fireball_spell">');
+	},
+	function() {
+		$(this).css('background-color', '#000');
+		$(this).css('color', '#fff');
+		$('#character_box').html('<img src="/img/front_facing.png" alt="character" id="character">');
+	}
+);
+$('#jquery').hover( 
+	function() {
+		$(this).css('background-color','#fff');
+		$(this).css('color', '#000');
+		$('#character_box').html('<img src="/img/poisoncloud_spell.gif" alt="character" id="spell_poisoncloud">');
+	},
+	function() {
+		$(this).css('background-color', '#000');
+		$(this).css('color', '#fff');
+		$('#character_box').html('<img src="/img/front_facing.png" alt="character" id="character">');
+	}
+);
+$('#php').hover( 
+	function() {
+		$(this).css('background-color','#fff');
+		$(this).css('color', '#000');
+		$('#character_box').html('<img src="/img/atomic_spell.gif" alt="character" id="spell_atomic">');
+	},
+	function() {
+		$(this).css('background-color', '#000');
+		$(this).css('color', '#fff');
+		$('#character_box').html('<img src="/img/front_facing.png" alt="character" id="character">');
+	}
+);
