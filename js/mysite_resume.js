f9559bf87a95ca7542f7c91ae7dac63b1d902898
David R. Jex <david.jex@me.com>
2017-07-17 19:00:28 -0500

    Finsihed resume page with animations on all languages known.

--- js/mysite_resume.js
+++ js/mysite_resume.js
@@ -51,7 +51,31 @@ $('#php').hover(
 	function() {
 		$(this).css('background-color','#fff');
 		$(this).css('color', '#000');
-		$('#character_box').html('<img src="/img/atomic_spell.gif" alt="character" id="spell_atomic">');
+		$('#character_box').html('<img src="/img/beam_spell.gif" alt="character" id="beam_spell">');
+	},
+	function() {
+		$(this).css('background-color', '#000');
+		$(this).css('color', '#fff');
+		$('#character_box').html('<img src="/img/front_facing.png" alt="character" id="character">');
+	}
+);
+$('#mysql').hover( 
+	function() {
+		$(this).css('background-color','#fff');
+		$(this).css('color', '#000');
+		$('#character_box').html('<img src="/img/teleportation_spell.gif" alt="character" id="spell_teleportation">');
+	},
+	function() {
+		$(this).css('background-color', '#000');
+		$(this).css('color', '#fff');
+		$('#character_box').html('<img src="/img/front_facing.png" alt="character" id="character">');
+	}
+);
+$('#laravel').hover( 
+	function() {
+		$(this).css('background-color','#fff');
+		$(this).css('color', '#000');
+		$('#character_box').html('<img src="/img/fire-djin_spell.gif" alt="character" id="spell_fire-djin">');
 	},
 	function() {
 		$(this).css('background-color', '#000');
