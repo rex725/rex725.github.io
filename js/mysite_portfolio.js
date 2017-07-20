cb1a2ad8336cbf1aa5e3656c9daf0cb6f0544ab6
David R. Jex <david.jex@me.com>
2017-07-20 13:36:36 -0500

    Introduction of portfolio page. Background added and syling for desktop view rough draft completed. Mobile view rough draft started.

---
+++ js/mysite_portfolio.js
@@ -0,0 +1,12 @@
+var windowWidth = $(window).width();
+
+if (windowWidth <= 500) {
+	$('body').html(
+		'<div><img src="/img/cave_entrances.png" class="cave_entrances"></div>' +
+		'<div><img src="/img/cave_entrances.png" class="cave_entrances"></div>' +
+		'<div><img src="/img/cave_entrances.png" class="cave_entrances"></div>' +
+		'<div><img src="/img/cave_entrances.png" class="cave_entrances"></div>' +
+		'<div><img src="/img/cave_entrances.png" class="cave_entrances"></div>' +
+		'<div><img src="/img/cave_entrances.png" class="cave_entrances"></div>'
+	);
+}
