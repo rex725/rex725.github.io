3df9d1563e858253be8dd506a8957c2432e3517f
David R. Jex <david.jex@me.com>
2017-07-25 18:03:12 -0500

    Updated time out transitions between pages once link is clicked so that mobile view doesn't wait as long.

--- js/mysite.js
+++ js/mysite.js
@@ -18,8 +18,23 @@ $('#resume').hover(function() {
 	$('#resume a').text('Resume');
 });
 $('#resume a').click(function(event) {
-	console.log('firing');
-		event.preventDefault();
+	var windowWidth = $(window).width();
+	event.preventDefault();
+	if (windowWidth <= 500) {
+		console.log('firing');
+		setTimeout(function() {
+			window.location.href = '/html/mysite_resume.html';
+		},3500);
+		$('.character_selector').html('<img src="/img/sword_swing_right.gif" id="actor_swinging_sword" alt="character">');
+		setTimeout(function() {
+			$('.character_selector').html('<img src="/img/walking_right.gif" id="actor_walking" alt="character">');
+			setInterval(function() {
+				$('#actor_walking').animate({
+						left: '+=30px'
+				});
+			}, 10);
+		}, 1000);
+	} else {
 		setTimeout(function() {
 			window.location.href = '/html/mysite_resume.html';
 		},9000);
@@ -31,12 +46,28 @@ $('#resume a').click(function(event) {
 						left: '+=30px'
 				});
 			}, 10);
-		},1000);
-		
+		}, 1000);
+	};		
 });
 $('#portfolio a').click(function(event) {
 	console.log('firing');
-		event.preventDefault();
+	var windowWidth = $(window).width();
+	event.preventDefault();
+	if (windowWidth <= 500) {
+		console.log('firing');
+		setTimeout(function() {
+			window.location.href = '/html/mysite_portfolio.html';
+		},3500);
+		$('.character_selector').html('<img src="/img/sword_swing_right.gif" id="actor_swinging_sword" alt="character">');
+		setTimeout(function() {
+			$('.character_selector').html('<img src="/img/walking_right.gif" id="actor_walking" alt="character">');
+			setInterval(function() {
+				$('#actor_walking').animate({
+						left: '+=30px'
+				});
+			}, 10);
+		}, 1000);
+	} else {
 		setTimeout(function() {
 			window.location.href = '/html/mysite_portfolio.html';
 		},9000);
@@ -48,8 +79,8 @@ $('#portfolio a').click(function(event) {
 						left: '+=30px'
 				});
 			}, 10);
-		},1000);
-		
+		}, 1000);
+	};		
 });
 $('#portfolio').hover(function() {
 	$('#portfolio a').text('Portfolio');
